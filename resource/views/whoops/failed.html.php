ef4db904967de0e6ae60f541b679bdc62ec0b104
cjj <cjj8110_202302@126.com>
2023-06-23 12:37:34 +0800

    fixed:0.2

--- resource/views/whoops/failed.html.php
+++ resource/views/whoops/failed.html.php
@@ -1,48 +1,12 @@
-<?php $view->load('whoops/layout');?>
-<?php $view->beginBlock('details');?>
-    <div class="details">
-        <h2 class="details-heading">Environment &amp; details:</h2>
-
-        <div class="data-table-container" id="data-tables">
-            <?php if(isset($tables)):?>
-                <?php foreach ($tables as $label => $data): ?>
-                    <div class="data-table" id="sg-<?php echo $view->escape($view->slug($label)); ?>">
-                        <?php if (!empty($data)): ?>
-                            <label><?php echo $view->escape($label); ?></label>
-                            <table class="data-table">
-                                <thead>
-                                <tr>
-                                    <td class="data-table-k">Key</td>
-                                    <td class="data-table-v">Value</td>
-                                </tr>
-                                </thead>
-                                <?php foreach ($data as $k => $value): ?>
-                                    <tr>
-                                        <td><?php echo $view->escape($k); ?></td>
-                                        <td><?php echo $value; ?></td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </table>
-                        <?php else: ?>
-                            <label class="empty"><?php echo $view->escape($label); ?></label>
-                            <span class="empty">empty</span>
-                        <?php endif; ?>
-                    </div>
-                <?php endforeach; ?>
-            <?php endif;?>
+<?php $view->extend('whoops/layout');?>
+<?php $view->beginBlock('content');?>
+    <div class="stack-container">
+        <div class="panel left-panel cf <?php echo (isset($has_frames)&&$has_frames ? 'empty' : ''); ?>">
+            <?php $view->load('whoops/panel_left'); ?>
         </div>
-
-        <?php /* List registered handlers, in order of first to last registered */ ?>
-        <div class="data-table-container" id="handlers">
-            <label>Registered Handlers</label>
-            <?php if(isset($handlers)):?>
-                <?php foreach ($handlers as $i => $h): ?>
-                    <div class="handler <?php echo ($h === $handler) ? 'active' : ''?>">
-                        <?php echo $i ?>. <?php echo $view->escape(get_class($h)) ?>
-                    </div>
-                <?php endforeach; ?>
-            <?php endif;?>
+        <div class="panel details-container cf">
+            <?php $view->load('whoops/frame_code'); ?>
+            <?php $view->load('whoops/details');?>
         </div>
-
     </div>
 <?php $view->endBlock();?>
--- resource/views/whoops/layout.html.php
+++ resource/views/whoops/layout.html.php
@@ -16,16 +16,7 @@
   <body>
 
     <div class="Whoops container">
-        <div class="stack-container">
-            <div class="panel left-panel cf <?php echo (isset($has_frames)&&$has_frames ? 'empty' : ''); ?>">
-                <?php $view->load('whoops/panel_left'); ?>
-            </div>
-            <div class="panel details-container cf">
-                <?php $view->load('whoops/frame_code'); ?>
-                <?php $view->place('details');?>
-            </div>
-        </div>
-
+        <?php $view->place('content');?>
     </div>
 
     <script src="/assets/js/prism.js"></script>
